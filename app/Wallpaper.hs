3deae4ee64bdbe0455646c167b8cf41cc68829e9
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2017-05-01 18:01:57 -0400

    rosette app

---
+++ app/Wallpaper.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Main where
+
+import           Juicy
+import           Recipes.Common
+import           Types
+
+import           Data.Complex
+import           Data.Yaml          (ParseException, decodeFileEither)
+import           System.Environment
+
+main :: IO ()
+main = do
+  [yamlFile] <- getArgs
+  (wp :: Either ParseException (Wallpaper Double)) <- decodeFileEither yamlFile
+  case wp of
+    Left e   -> error (show e)
+    Right w  -> wallpaper w
+
+coefs :: [Coef Double]
+coefs = [ Coef 1 0 (0.75:+0.25)
+        , Coef (-2) 2 (0.2:+(-0.2))
+        , Coef 1 (-1) (0.6:+0.1)
+        ]
+
+wallpaper :: RealFloat a => Wallpaper a -> IO ()
+wallpaper wp = symmetryPattern (wpOptions wp)
+                               (recipe (wpGroup wp))
+                               (wpCoefs wp)
+                               (wpType wp)
+                               (wpProcess wp)
+                               (wpWheel wp)
+                               (wpPath wp)
